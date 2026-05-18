13851caa7c46eaf440c3069dd514d7815924da92
farhanje <farhan.fauzan@astrapay.com>
2026-05-18 14:48:27 +0700

    Add Post schema

---
+++ sanity/schemas/post.js
@@ -0,0 +1,24 @@
+export default {
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    { name: 'title', title: 'Title', type: 'string', validation: (Rule) => Rule.required() },
+    { name: 'slug', title: 'Slug', type: 'slug', options: { source: 'title', maxLength: 96 }, validation: (Rule) => Rule.required() },
+    { name: 'publishedAt', title: 'Published at', type: 'datetime' },
+    { name: 'excerpt', title: 'Excerpt', type: 'text', rows: 3 },
+    { name: 'tags', title: 'Tags', type: 'array', of: [{ type: 'string' }] },
+    {
+      name: 'body',
+      title: 'Body',
+      type: 'array',
+      of: [
+        { type: 'block' },
+        { type: 'image', options: { hotspot: true } },
+      ],
+    },
+  ],
+  preview: {
+    select: { title: 'title', subtitle: 'publishedAt' },
+  },
+}
