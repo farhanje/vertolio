486ff4ba9f604a3d3f4d0df688661445a6e44bba
farhanje <farhan.fauzan@astrapay.com>
2026-05-18 14:46:54 +0700

    Add schemaTypes index

---
+++ sanity/schemaTypes.js
@@ -0,0 +1,6 @@
+import organization from './schemas/organization'
+import project from './schemas/project'
+import post from './schemas/post'
+import siteSettings from './schemas/siteSettings'
+
+export const schemaTypes = [siteSettings, organization, project, post]
