35eacac2671c61dcdb3004a3e67d704eaa1b42da
farhanje <farhan.fauzan@astrapay.com>
2026-05-18 15:05:20 +0700

    Avoid Vercel SSG timeouts: force dynamic rendering

--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,7 +1,7 @@
 import {sanity} from '../../lib/sanity.client'
 import {ORGANIZATIONS_QUERY, WORK_INDEX_QUERY} from '../../lib/sanity.queries'
 
-export const revalidate = 60
+export const dynamic = 'force-dynamic'
 
 function groupByOrg(items = []) {
   const map = new Map()
