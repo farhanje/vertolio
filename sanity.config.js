fce2baeed7f057b7006edc6fabae20baf7687a66
farhanje <farhan.fauzan@astrapay.com>
2026-05-18 14:03:03 +0700

    Add sanity.config.js (Studio config)

---
+++ sanity.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './sanity/schemaTypes';
+
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+
+export default defineConfig({
+  name: 'default',
+  title: 'Vertolio Studio',
+  projectId,
+  dataset,
+  plugins: [deskTool(), visionTool()],
+  schema: { types: schemaTypes },
+});
