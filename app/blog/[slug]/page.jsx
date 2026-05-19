e55950d9c77591af5d78811eeb24d33471ff55dd
farhanje <farhan.fauzan@astrapay.com>
2026-05-19 11:57:18 +0700

    Blog post: apply accent per post (or default from Site Settings)

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,5 +1,6 @@
 import {sanityFetch} from '../../../lib/sanity.client'
-import {POST_BY_SLUG_QUERY} from '../../../lib/sanity.queries'
+import {SITE_SETTINGS_QUERY, POST_BY_SLUG_QUERY} from '../../../lib/sanity.queries'
+import {placeholderSiteSettings} from '../../../lib/placeholders'
 import {RichText} from '../../../lib/portableText'
 import Toc from '../../../components/Toc'
 
@@ -7,7 +8,10 @@ export const dynamic = 'force-dynamic'
 export const fetchCache = 'force-no-store'
 
 export default async function BlogPost({ params }) {
-  const post = await sanityFetch(POST_BY_SLUG_QUERY, { slug: params.slug })
+  const [settings, post] = await Promise.all([
+    sanityFetch(SITE_SETTINGS_QUERY).catch(() => placeholderSiteSettings),
+    sanityFetch(POST_BY_SLUG_QUERY, { slug: params.slug }),
+  ])
 
   if (!post) {
     return (
@@ -21,8 +25,11 @@ export default async function BlogPost({ params }) {
     )
   }
 
+  const defaultAcc = settings?.pageAccents?.postDefault || 'none'
+  const accent = post?.accent && post.accent !== 'default' ? post.accent : defaultAcc
+
   return (
-    <main className="container">
+    <main className="container" data-accent={accent}>
       <section className="section tight">
         <div className="grid12">
           <div style={{ gridColumn: '1 / span 8' }}>
