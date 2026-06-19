8175e57e31fdba06654d314397273bbb891068b7
farhanje <farhan.fauzan@astrapay.com>
2026-06-19 09:24:44 +0700

    Register research study schema in Sanity Studio

--- sanity/schemaTypes.js
+++ sanity/schemaTypes.js
@@ -2,5 +2,6 @@ import organization from './schemas/organization'
 import project from './schemas/project'
 import post from './schemas/post'
 import siteSettings from './schemas/siteSettings'
+import researchStudy from './schemaTypes/researchStudy'
 
-export const schemaTypes = [siteSettings, organization, project, post]
+export const schemaTypes = [siteSettings, organization, project, post, researchStudy]
