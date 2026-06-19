ab11dafc461042b3c374210c83687132b73506ce
farhanje <farhan.fauzan@astrapay.com>
2026-06-19 09:05:47 +0700

    Register research study schema

--- sanity/schemaTypes/index.js
+++ sanity/schemaTypes/index.js
@@ -2,5 +2,6 @@ import siteSettings from './siteSettings';
 import organization from './organization';
 import project from './project';
 import post from './post';
+import researchStudy from './researchStudy';
 
-export const schemaTypes = [siteSettings, organization, project, post];
+export const schemaTypes = [siteSettings, organization, project, post, researchStudy];
