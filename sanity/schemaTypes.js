797f97a86d67389653c86a14229617c1bdb06b07
farhanje <farhan.fauzan@astrapay.com>
2026-08-22 21:43:22 +0700

    Register recruiter link schema

--- sanity/schemaTypes.js
+++ sanity/schemaTypes.js
@@ -2,6 +2,7 @@ import organization from './schemas/organization'
 import project from './schemas/project'
 import post from './schemas/post'
 import siteSettings from './schemas/siteSettings'
+import recruiterLink from './schemas/recruiterLink'
 import researchStudy from './schemaTypes/researchStudy'
 
-export const schemaTypes = [siteSettings, organization, project, post, researchStudy]
+export const schemaTypes = [siteSettings, organization, project, post, recruiterLink, researchStudy]
