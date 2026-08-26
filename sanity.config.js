ada2a968c15fd56510f9952b4e9b862c6a2ac829
farhanje <farhan.fauzan@astrapay.com>
2026-08-26 17:00:21 +0700

    Render case study array items inline in Sanity Studio

--- sanity.config.js
+++ sanity.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'sanity';
 import { deskTool } from 'sanity/desk';
 import { visionTool } from '@sanity/vision';
 import { schemaTypes } from './sanity/schemaTypes';
+import InlineCaseStudyArrayItem from './sanity/components/InlineCaseStudyArrayItem';
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
@@ -54,4 +55,9 @@ export default defineConfig({
 
   plugins: [deskTool({ structure }), visionTool()],
   schema: { types: schemaTypes },
+  form: {
+    components: {
+      item: InlineCaseStudyArrayItem,
+    },
+  },
 });
