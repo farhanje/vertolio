916fe58615db1f3c4a3512e48c2004b57c40f895
farhanje <farhan.fauzan@astrapay.com>
2026-08-26 17:57:18 +0700

    Fix standalone Studio CLI project config

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,7 +1,13 @@
 import {defineCliConfig} from 'sanity/cli'
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production'
+const projectId =
+  process.env.SANITY_STUDIO_PROJECT_ID ||
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  'iq6vjwu7'
+const dataset =
+  process.env.SANITY_STUDIO_DATASET ||
+  process.env.NEXT_PUBLIC_SANITY_DATASET ||
+  'production'
 
 export default defineCliConfig({
   api: {
